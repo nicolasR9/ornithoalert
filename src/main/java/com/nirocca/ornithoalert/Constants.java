2ecdbd69c1e55acd48eb9b50dc94d446e6647500
Nicolas Rocca <nirocca@paypal.com>
2018-04-12 16:58:39 +0200

    small updates

--- src/main/java/com/nirocca/ornithoalert/Constants.java
+++ src/main/java/com/nirocca/ornithoalert/Constants.java
@@ -50,15 +50,14 @@ public class Constants {
     
     // see https://www.w3schools.com/colors/colors_names.asp
     static {
+        LATIN_NAME_TO_COLOR.put("Anthus trivialis", "sienna"); //Baumpieper
         LATIN_NAME_TO_COLOR.put("Gallinago gallinago", "darkblue"); //Bekassine
         LATIN_NAME_TO_COLOR.put("Aythya marila", "lightgreen"); //Bergente
-        LATIN_NAME_TO_COLOR.put("Fringilla montifringilla", "darkgreen"); //Bergfink
         LATIN_NAME_TO_COLOR.put("Anthus spinoletta", "lightblue"); //Bergpieper
         LATIN_NAME_TO_COLOR.put("Lullula arborea", "salmon"); //Haubenlaerche
         LATIN_NAME_TO_COLOR.put("Columba oenas", "yellow"); //Hohltaube
         LATIN_NAME_TO_COLOR.put("Anas querquedula", "teal"); //Knaeckente
         LATIN_NAME_TO_COLOR.put("Buteo lagopus", "sandybrown"); //Raufussbussard
-        LATIN_NAME_TO_COLOR.put("Podiceps nigricollis", "sienna"); //Schwarzhalstaucher
         LATIN_NAME_TO_COLOR.put("Larus cachinnans", "aquamarine"); //Steppenmoewe
         LATIN_NAME_TO_COLOR.put("Certhia familiaris", "orange"); //Waldbaumlaeufer
         LATIN_NAME_TO_COLOR.put("Scolopax rusticola", "beige"); //Waldschnaepfe
@@ -67,7 +66,7 @@ public class Constants {
         LATIN_NAME_TO_COLOR.put("Anthus pratensis", "wheat"); //Wiesenpieper
         LATIN_NAME_TO_COLOR.put("Cygnus bewickii", "violet"); //Zwergschwan
         
-        //free: darkblue, teal
+        //free:  darkgreen
     }
     
     public static enum SortBy {
