6bf837273b8f2749e5063610ab1edb56a176cfa2
nirocca <belanglos0815@gmx.de>
2025-03-10 12:52:32 +0100

    seawatching

--- src/main/java/com/nirocca/ornithoalert/location/SpecificSpeciesMain.java
+++ src/main/java/com/nirocca/ornithoalert/location/SpecificSpeciesMain.java
@@ -1,21 +1,5 @@
 package com.nirocca.ornithoalert.location;
 
-import static com.nirocca.ornithoalert.Species.ALPENBRAUNELLE;
-import static com.nirocca.ornithoalert.Species.DREIZEHENSPECHT;
-import static com.nirocca.ornithoalert.Species.GRAUSPECHT;
-import static com.nirocca.ornithoalert.Species.HASELHUHN;
-import static com.nirocca.ornithoalert.Species.KLEINES_SUMPFHUHN;
-import static com.nirocca.ornithoalert.Species.REBHUHN;
-import static com.nirocca.ornithoalert.Species.SANDERLING;
-import static com.nirocca.ornithoalert.Species.SCHWARZKOPFMOEWE;
-import static com.nirocca.ornithoalert.Species.SICHELSTRANDLAEUFER;
-import static com.nirocca.ornithoalert.Species.TANNENHAEHER;
-import static com.nirocca.ornithoalert.Species.TEMMINCKSTRANDLAEUFER;
-import static com.nirocca.ornithoalert.Species.TUEPFELSUMPFHUHN;
-import static com.nirocca.ornithoalert.Species.WEISSRUECKENSPECHT;
-import static com.nirocca.ornithoalert.Species.ZWERGDOMMEL;
-import static com.nirocca.ornithoalert.Species.ZWERGSTRANDLAEUFER;
-
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayDeque;
@@ -36,31 +20,26 @@ import com.nirocca.ornithoalert.PrintParameters;
 import com.nirocca.ornithoalert.Species;
 import com.nirocca.ornithoalert.model.Sighting;
 
+import static com.nirocca.ornithoalert.Species.*;
+
 public class SpecificSpeciesMain {
 
     //private static final String URL_TEMPLATE = "https://www.ornitho.de/index.php?m_id=94&p_c=duration&p_cc=206&sp_tg=1&sp_DFrom=02.07.2024&sp_DTo=02.07.2024&sp_DSeasonFromDay=1&sp_DSeasonFromMonth=1&sp_DSeasonToDay=31&sp_DSeasonToMonth=12&sp_DChoice=offset&sp_DOffset=30&sp_SChoice=species&speciesFilter=&sp_S=341&sp_Cat%5Bnever%5D=1&sp_Cat%5Bveryrare%5D=1&sp_Cat%5Brare%5D=1&sp_Cat%5Bunusual%5D=1&sp_Family=1&sp_PChoice=canton&sp_cC=0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000&p_cc=206&sp_CommuneCounty=356&sp_Commune=12332&sp_Info=&sp_Polygon=&sp_PolygonSaveName=&sp_PolygonSaveRestoreID=&sp_AltitudeFrom=-19&sp_AltitudeTo=2962&sp_CommentValue=&sp_OnlyAH=0&sp_Ats=-00000&sp_project=&sp_OnlyStoc=&sp_frmListType=&sp_FChoice=list&sp_FDisplay=DATE_PLACE_SPECIES&sp_DFormat=DESC&sp_FOrderListSpecies=COUNT&sp_FListSpeciesChoice=DATA&sp_DateSynth=02.07.2024&sp_FOrderSynth=ALPHA&sp_FGraphChoice=DATA&sp_FGraphFormat=auto&sp_FAltScale=250&sp_FAltChoice=DATA&sp_FMapFormat=none&submit=Abfrage+starten";
-    private static final String URL_TEMPLATE = "https://www.ornitho.de/index.php?m_id=94&p_c=places&p_cc=213&sp_tg=1&sp_DChoice=range&sp_DFrom=26.05.2024&sp_DTo=26.05.2024&sp_DSeasonFromDay=1&sp_DSeasonFromMonth=1&sp_DSeasonToDay=31&sp_DSeasonToMonth=12&sp_DOffset=5&sp_SChoice=species&speciesFilter=&sp_S=326&sp_Cat%5Bnever%5D=1&sp_Cat%5Bveryrare%5D=1&sp_Cat%5Brare%5D=1&sp_Cat%5Bunusual%5D=1&sp_Family=1&sp_PChoice=canton&sp_cC=0000000000000000000000000000000000000000000000000000000000010000000000000010000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000&p_cc=213&sp_CommuneCounty=356&sp_Commune=12332&sp_Info=&sp_Polygon=&sp_PolygonSaveName=&sp_PolygonSaveRestoreID=&sp_AltitudeFrom=-19&sp_AltitudeTo=2962&sp_CommentValue=&sp_OnlyAH=0&sp_Ats=-00000&sp_project=&sp_OnlyStoc=&sp_frmListType=&sp_FChoice=list&sp_FDisplay=DATE_PLACE_SPECIES&sp_DFormat=DESC&sp_FOrderListSpecies=ALPHA&sp_FListSpeciesChoice=DATA&sp_DateSynth=10.11.2022&sp_FOrderSynth=ALPHA&sp_FGraphChoice=DATA&sp_FGraphFormat=auto&sp_FAltScale=250&sp_FAltChoice=DATA&sp_FMapFormat=none&submit=Abfrage+starten";
-    private static final int[] yearsToCheck = {2021, 2022, 2023, 2024};
-    private static final String FROM_DATE = "01.01.";
-    private static final String TO_DATE = "31.12.";
-
-    private static final Species[] SPECIES1 = {
-        DREIZEHENSPECHT,
-        SCHWARZKOPFMOEWE,
-        WEISSRUECKENSPECHT,
-        TANNENHAEHER,
-        TEMMINCKSTRANDLAEUFER,
-        GRAUSPECHT,
-        HASELHUHN,
-        SANDERLING,
-        SICHELSTRANDLAEUFER,
-        TUEPFELSUMPFHUHN,
-        ZWERGSTRANDLAEUFER,
-        REBHUHN,
-        ZWERGDOMMEL,
-        KLEINES_SUMPFHUHN};
+    private static final String URL_TEMPLATE = "https://www.ornitho.de/index.php?m_id=94&p_c=places&p_cc=203&sp_tg=1&sp_DChoice=range&sp_DFrom=26.05.2024&sp_DTo=26.05.2024&sp_DSeasonFromDay=1&sp_DSeasonFromMonth=1&sp_DSeasonToDay=31&sp_DSeasonToMonth=12&sp_DOffset=5&sp_SChoice=species&speciesFilter=&sp_S=326&sp_Cat%5Bnever%5D=1&sp_Cat%5Bveryrare%5D=1&sp_Cat%5Brare%5D=1&sp_Cat%5Bunusual%5D=1&sp_Family=1&sp_PChoice=canton&sp_cC=000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000111111111111111111111111111111111111111111111100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000011111111111111110000000000000000000000000000000000&p_cc=203&sp_CommuneCounty=356&sp_Commune=12332&sp_Info=&sp_Polygon=&sp_PolygonSaveName=&sp_PolygonSaveRestoreID=&sp_AltitudeFrom=-19&sp_AltitudeTo=2962&sp_CommentValue=&sp_OnlyAH=0&sp_Ats=-00000&sp_project=&sp_OnlyStoc=&sp_frmListType=&sp_FChoice=list&sp_FDisplay=DATE_PLACE_SPECIES&sp_DFormat=DESC&sp_FOrderListSpecies=ALPHA&sp_FListSpeciesChoice=DATA&sp_DateSynth=10.11.2022&sp_FOrderSynth=ALPHA&sp_FGraphChoice=DATA&sp_FGraphFormat=auto&sp_FAltScale=250&sp_FAltChoice=DATA&sp_FMapFormat=none&submit=Abfrage+starten";
+    private static final int[] yearsToCheck = {2020, 2021, 2022, 2023, 2024};
+    private static final String FROM_DATE = "15.08.";
+    private static final String TO_DATE = "10.11.";
 
     private static final Species[] SPECIES = {
+        SCHWALBENMOEWE,
+        SPATELRAUBMOEWE,
+        SKUA,
+        WELLENLAEUFER,
+        KRABBENTAUCHER,
+        DUNKLER_STURMTAUCHER,
+        ATLANTIKSTURMTAUCHER};
+
+    private static final Species[] SPECIES1 = {
         DREIZEHENSPECHT,
         WEISSRUECKENSPECHT,
         TANNENHAEHER,
